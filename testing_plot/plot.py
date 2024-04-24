70cb1f88b74a02457dab1af780e5b4763d6b296d
Didi0dum <Deedee.vasileva@gmail.com>
2024-04-24 20:12:37 +0300

    only fft plot + libs for uart

--- testing_plot/plot.py
+++ testing_plot/plot.py
@@ -1,16 +1,11 @@
 from matplotlib import *
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Test time signal
-
-Fs = 2000  # sampling freq
-tstep = 1/Fs
-f0 = 100
-N = int(10 * Fs / f0)
-from matplotlib import *
-import matplotlib.pyplot as plt
-import numpy as np
+from gettext import npgettext
+import struct
+import serial
+from matplotlib.animation import FuncAnimation 
+from threading import Thread
 
 # Test time signal
 
@@ -33,8 +28,8 @@ x_mag_plot = 2 * X_mag[0:int(N/2+1)]
 x_mag_plot[0] = x_mag_plot[0] / 2
 
 
-fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
-ax1.plot(t, y, '.-')
+fig, ax2 = plt.subplots(nrows=1, ncols=1)
+#ax1.plot(t, y, '.-')
 ax2.plot(f_plot, x_mag_plot, '.-')
 plt.show()
 t = np.linspace(0, (N -1)*tstep, N)
@@ -55,3 +50,4 @@ fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
 ax1.plot(t, y, '.-')
 ax2.plot(f_plot, x_mag_plot, '.-')
 plt.show()
+

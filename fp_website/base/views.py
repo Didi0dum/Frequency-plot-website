dda9e12a36bdf1d0137b9bcdd6f648c7dc36c1b0
Didi0dum <Deedee.vasileva@gmail.com>
2024-05-25 10:28:20 +0300

    for no real reason it now renders what it needs to

--- fp_website/base/views.py
+++ fp_website/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+# Create your views here.
+
 def authView(request):
     form = UserCreationForm()
-    return render(request, "registrarion/signup.html", {"form": form})
+    return render(request, "registration/signup.html", {"form": form})

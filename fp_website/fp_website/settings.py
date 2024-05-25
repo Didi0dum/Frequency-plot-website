6bc45265fe78290bcf77c65f9020a4bbff942534
Didi0dum <Deedee.vasileva@gmail.com>
2024-05-25 12:20:29 +0300

    login and signup (not pretty but it works idc)
    I need to connect matplotlib API to this shit RAAAAAAAAH

--- fp_website/fp_website/settings.py
+++ fp_website/fp_website/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
